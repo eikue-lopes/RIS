3dba7f067c6df0ba7d80e796ea5d93956aa51783
eikue lopes <eikuelopes2014@gmail.com>
2020-08-05 17:14:34 -0300

    updates

--- scripts/main.gd
+++ scripts/main.gd
@@ -10,9 +10,6 @@ func _on_btn_play_pressed():
 	get_node("gui/btn_play").play()
 	game_paused = false
 
-func play_game():
-	_on_btn_play_pressed()
-
 func _on_btn_restart_pressed():
 	get_node("gui/btn_restart").restart()
 	game_paused = false
@@ -21,17 +18,8 @@ func _on_btn_pause_pressed():
 	get_node("gui/btn_pause").pause()
 	game_paused = true
 
-func pause_game():
-	_on_btn_pause_pressed()
 
 func _process(delta):
 	if game.lifes == 0:
 		get_node("gui/game_over_label").game_over()
-	
-	elif Input.is_action_just_pressed("play_pause"):
-		if game_paused:
-			play_game()
-		else:
-			pause_game()
-
 
